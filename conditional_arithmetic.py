e54794f1ae8928db63ff7672efba9991df0baccc
AbhineelV <167945284+AbhineelV@users.noreply.github.com>
2024-07-09 21:56:18 +0530

    Add files via upload

---
+++ conditional_arithmetic.py
@@ -0,0 +1,9 @@
+def num(val1,val2):
+    if(val1>val2):
+        result = val1 + val2
+        print(f'val1 + val2 ={result}')
+    else:
+        result1=val1 * val2
+        print(f'val1 * val2 ={result1}')
+num(20 ,5)
+num(50,120)
